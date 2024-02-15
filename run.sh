41f698c6546c63dd1208ce1ecbd094f839b3187a
Kai0139 <zkrs3914@outlook.com>
2024-02-16 00:45:47 +0800

    save progress

--- run.sh
+++ run.sh
@@ -2,4 +2,4 @@
 IMG_DIR=/home/user/Depth-Anything/input
 OUT_DIR=/home/kaizhang/data/mini_allweather/feature_viz/input
 
-python3 visualize_features.py --encoder vits --img-path ${IMG_DIR} --outdir ${OUT_DIR}
+python3 visualize_features_all.py --encoder vits --img-path ${IMG_DIR} --outdir ${OUT_DIR}

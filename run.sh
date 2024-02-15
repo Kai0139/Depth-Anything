7d0263e2e9ddd14e764163370fe95c6261816545
Kai0139 <zkrs3914@outlook.com>
2024-02-15 22:17:18 +0800

    visualize features from dino1

--- run.sh
+++ run.sh
@@ -2,4 +2,4 @@
 IMG_DIR=/home/zhangkai/data/mini_allweather/input
 OUT_DIR=/home/zhangkai/data/mini_allweather/feature_viz/input
 
-python3 visualize_features.py --encoder vits --img-path ${IMG_DIR} --outdir ${OUT_DIR}
+python3 visualize_features_dino1.py --encoder vits --img-path ${IMG_DIR} --outdir ${OUT_DIR}

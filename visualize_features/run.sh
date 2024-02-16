71f072cb10374f060c06ca16703d47b44180fe2b
Kai0139 <zkrs3914@outlook.com>
2024-02-16 21:21:20 +0800

    save run script

--- visualize_features/run.sh
+++ visualize_features/run.sh
@@ -8,9 +8,9 @@ OUT_DIR=$script_dir/data
 DATA_DIR=${OUT_DIR}
 RESULT_DIR=$script_dir/results
 
-# python3 visualize_features_dinov2.py --encoder vits --resizeh 1554 --img-path ${IMG_DIR} --outdir ${OUT_DIR}
-# python3 visualize_features_dino1.py --encoder vits --resizeh 1554 --img-path ${IMG_DIR} --outdir ${OUT_DIR}
-# python3 visualize_features_dpt.py --encoder vits --resizeh 1554 --img-path ${IMG_DIR} --outdir ${OUT_DIR}
-# python3 visualize_features_vgg.py --encoder vits --resizeh 2072 --img-path ${IMG_DIR} --outdir ${OUT_DIR}
+python3 visualize_features_dinov2.py --encoder vits --resizeh 1554 --img-path ${IMG_DIR} --outdir ${OUT_DIR}
+python3 visualize_features_dino1.py --encoder vits --resizeh 1554 --img-path ${IMG_DIR} --outdir ${OUT_DIR}
+python3 visualize_features_dpt.py --encoder vits --resizeh 1554 --img-path ${IMG_DIR} --outdir ${OUT_DIR}
+python3 visualize_features_vgg.py --encoder vits --resizeh 3108 --img-path ${IMG_DIR} --outdir ${OUT_DIR}
 
 python3 merge_plots.py --imgdir ${IMG_DIR} --datadir ${DATA_DIR} --outdir ${RESULT_DIR}
